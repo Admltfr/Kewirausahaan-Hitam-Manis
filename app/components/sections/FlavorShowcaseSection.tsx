9cdaf3c9f58edb8c3c2a9b576fd238a290a10a1e
admltfr <adamlutfiramadhan@gmail.com>
2026-04-19 14:56:31 +0700

    feat: add product image

--- app/components/sections/FlavorShowcaseSection.tsx
+++ app/components/sections/FlavorShowcaseSection.tsx
@@ -7,19 +7,19 @@ const products = [
   {
     title: "Churros + Chocolate",
     description: "Klasik, rich, dan selalu jadi favorit untuk recharge energi.",
-    image: "/images/churros-chocolate.svg",
+    image: "/images/churros.jpg",
   },
   {
     title: "Churros + Matcha",
     description:
       "Perpaduan manis dan earthy yang bikin rasa lebih berkarakter.",
-    image: "/images/churros-matcha.svg",
+    image: "/images/churros.jpg",
   },
   {
     title: "Churros + Strawberry",
     description:
       "Manis segar dengan sentuhan fruity yang fun di setiap gigitan.",
-    image: "/images/churros-strawberry.svg",
+    image: "/images/churros.jpg",
   },
 ];
 
